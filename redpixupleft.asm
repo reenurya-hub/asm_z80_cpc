b94a423f1f1e1fb62da98edd877ca51b1f2ea78b
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2022-08-22 19:48:14 -0500

    Add files via upload

--- redpixupleft.asm
+++ redpixupleft.asm
@@ -1,5 +1,5 @@
-org &4000
-run &4000
+org &4000     ;start of program
+run &4000     ;point of run program
 ld a,#88      ;3e 88        (4000)
 ld (#c000),a  ;32 00 c0     (4002)
-jr #4000      ;18 fe        (4005)
+jr #4005      ;18 fe        (4005)
