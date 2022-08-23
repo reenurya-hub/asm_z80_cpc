142389f79f692f16f786a738c84185ed288f1950
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2022-08-23 08:04:18 -0500

    Delete restaconcarry2.asm

--- restaconcarry2.asm
+++
@@ -1,7 +0,0 @@
-org &4000
-run &4000
-;; En este caso el valor de restar #01 - #ff es - #FE
-ld a,#01		;;guarda en A valor #01
-sbc #ff			;; resta con carry a A el valor #FF
-cpl			;; invierte el valor de A, resultado FE
-jr $
