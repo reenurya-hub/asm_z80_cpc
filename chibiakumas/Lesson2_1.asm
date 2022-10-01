21d8b67f4e9540ae6903aff71000f73f6de5fa9a
Reinaldo Urquijo <reinaldo.urquijo@gmail.com>
2022-09-30 20:03:10 -0500

    Delete chibiakumas directory

--- chibiakumas/Lesson2_1.asm
+++
@@ -1,7 +0,0 @@
-org &8000
-run &8000
-   ld hl, &0000		;; initial position to copy bytes
-   ld de, &c000		;; destination position (memory video) to copy bytes
-   ld bc, &4000		;; 'times' to copy from hl to de (memory video size 16Kb)
-   ldir			;; load (hl), increment (hl,de), repeat (bc)
-ret
--- chibiakumas/Lesson2_2.asm
+++
@@ -1,9 +0,0 @@
-ScreenSize equ &4000	;;define symbol or constant ScreenSize value &4000
-org &8100		
-run &8100
-   ld hl, &c000		;; initial position of hl
-   ld de, &c000+1	;; destination to copy bytes (add constant value 1)
-   ld bc, ScreenSize-1	;; 'times' to repeat loads (substract constant value 1 to ScreenSize)
-   ld (hl), 0		;; load value 0x00 in memory video (clear pixels)
-   ldir			;; load, increment, repeat
-jr $			;; jump to same position to jr $
